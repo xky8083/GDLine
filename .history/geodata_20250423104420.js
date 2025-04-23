fc9b596ca874e14ba5d0477fc89929da73724a02
xky8083 <77953876@qq.com>
2025-04-23 13:20:46 +0800

    天地图

---
+++ .history/geodata_20250423104420.js
@@ -0,0 +1,13 @@
+var points = {
+    "type":"FeatureCollection"
+    ,"features":[
+        {"type":"Feature","properties":{"name":"Point 1","introduce":"这是第1个点","photo":"./img/5/JPEG_20250421_093030.jpg"},"geometry":{"type":"Point","coordinates":[120.1443084,35.92873361]}},
+        {"type":"Feature","properties":{"name":"Point 2","introduce":"这是第2个点","photo":"./img/6/JPEG_20250421_093154.jpg"},"geometry":{"type":"Point","coordinates":[120.143826909218,35.9283061323443]}},    
+        {"type":"Feature","properties":{"name":"Point 3","introduce":"这是第3个点","photo":"./img/7/JPEG_20250421_093258.jpg"},"geometry":{"type":"Point","coordinates":[120.1432662,35.9279775]}},
+        {"type":"Feature","properties":{"name":"Point 4","introduce":"这是第4个点","photo":"./img/8/JPEG_20250421_093513.jpg"},"geometry":{"type":"Point","coordinates":[120.1429161,35.9278408]}},
+        {"type":"Feature","properties":{"name":"Point 5","introduce":"这是第5个点","photo":"./img/9/JPEG_20250421_093935.jpg"},"geometry":{"type":"Point","coordinates":[120.1423944,35.9276708]}},
+        {"type":"Feature","properties":{"name":"Point 38","introduce":"这是第38个点","photo":"./img/38/JPEG_20250421_110801.jpg"},"geometry":{"type":"Point","coordinates":[120.1249818,35.9230217]}},
+   
+    ]};
+
+var line = {"type":"Feature","id":"01","properties":{"name":"Line 1","introduce":"这是第1条线"},"geometry":{"type":"LineString","coordinates":[[-73.935242,40.730610],[-74.0060,40.7128],[-73.9857,40.7484]]}};
